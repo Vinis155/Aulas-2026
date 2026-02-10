097bf1975f923c6feea240cccc3eb58726359c20
Vinis155 <vinis155.vs@gmail.com>
2026-02-10 19:01:19 -0300

    feat: adiciona ML-CHATBOT com arquivos

---
+++ Primeiro semestre/ML-CHATBOT/check_env.py
@@ -0,0 +1,11 @@
+import sys
+import pandas as pd
+import matplotlib
+import sklearn
+
+print("PYTHON:", sys.version)
+print("PANDAS:", pd.__version__)
+print("MATPLOTLIB:", matplotlib.__version__)
+print("SKLEARN:", sklearn.__version__)
+
+print("\nRESULTADO: PRONTO PARA AULA!")
