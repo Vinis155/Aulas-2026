3409e7b49613ac8a8db103345933fad231dbf97f
Vinis155 <vinis155.vs@gmail.com>
2026-03-17 20:52:13 -0300

    Mobile&Game

--- Primeiro semestre/Game-Developmente/aula-sex---pong-20260213T220410Z-1-001/antonio---platform/scripts/player.gd
+++ Primeiro semestre/Game-Developmente/aula-sex---pong-20260213T220410Z-1-001/antonio---platform/scripts/player.gd
@@ -1,1 +1,27 @@
 extends CharacterBody2D
+
+var speed = 160
+var jump_velocity = -400
+var gravity = 980
+
+var max_jumps = 2
+var extra_jumps = 2
+
+func _physics_process(delta):
+
+	var dir = Input.get_axis("left", "right")
+	velocity.x = dir * speed
+
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	else:
+		extra_jumps = max_jumps
+
+	if Input.is_action_just_pressed("jump") and extra_jumps > 0:
+		velocity.y = jump_velocity
+		extra_jumps -= 1
+
+	if is_on_floor():
+		extra_jumps = 2
+	
+	move_and_slide()
